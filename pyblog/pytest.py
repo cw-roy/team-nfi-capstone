4c43048f1c9c6b4af86f2de31dbdf7b137a6ef42
Shane Ray <ShaneRay.Dev@Gmail.com>
2022-12-09 09:38:36 -0600

    update to pytest.py:w

--- pyblog/pytest.py
+++ pyblog/pytest.py
@@ -1,12 +1,23 @@
 import requests
 import os
 
-prod_wp_url = os.environ.get('WP_HOST') #stored in an env
 
-def test_wp_site():
+prod_wp_url = os.environ.get('WP_HOST') #stored in an env
+stage_wp_url = os.environ.get('WP_STAGE') #stored in an env
+
+def prod_wp_site():
     r = requests.get(prod_wp_url)
     if r.status_code == 200:
         print("Prod WordPress server is active")
     else:
         print("Wordpress server may need attention")
-test_wp_site()
+prod_wp_site()
+
+
+def stage_wp_site():
+    r = requests.get(prod_wp_url)
+    if r.status_code == 200:
+        print("Staging WordPress server is active")
+    else:
+        print("The Staging Wordpress server may need attention")
+stage_wp_site()
